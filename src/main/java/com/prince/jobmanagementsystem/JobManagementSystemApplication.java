7cd001530f31c96a0f10b14d70adfca70589e3b1
PrinceDickson <princed@decagonhq.com>
2021-08-23 22:29:42 +0100

    setup

---
+++ src/main/java/com/prince/jobmanagementsystem/JobManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.prince.jobmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobManagementSystemApplication.class, args);
+	}
+
+}
